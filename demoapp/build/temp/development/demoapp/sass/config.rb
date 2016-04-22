9065e5dc69dde1213ce6ae595105307a81b7a53c
Daniel Bruce <danieljbruce@gmail.com>
2016-04-22 14:38:10 -0400

    First commit

---
+++ demoapp/build/temp/development/demoapp/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
+cache_path = 'C:\Users\Daniel\Google Drive\Programming\Github\demoapp/build/.sass-cache'
---
+++ demoapp/build/temp/development/demoapp/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\Daniel\Google Drive\Programming\Github\demoapp\app.js
